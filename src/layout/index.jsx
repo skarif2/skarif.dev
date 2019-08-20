3b59bd83290805ffea506ec24d08e38ccbf9c323
Sk Arif <skarif2@gmail.com>
2019-08-20 21:32:43 +0600

    fixing loading issue

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
+import { config } from "@fortawesome/fontawesome-svg-core"
 
 import ThemeContext from "../context/ThemeContext"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import config from "../../data/SiteConfig"
+import siteConfig from "../../data/SiteConfig"
 import "../styles/main.scss"
+
+config.autoAddCss = false
 
 const Layout = props => {
   const { children } = props
@@ -18,9 +21,9 @@ const Layout = props => {
           class: `${dark ? "dark" : "light"}`,
         }}
       >
-        <meta name="description" content={config.siteDescription} />
+        <meta name="description" content={siteConfig.siteDescription} />
       </Helmet>
-      <Navbar navItems={config.navItems} />
+      <Navbar navItems={siteConfig.navItems} />
       <main id="main-content">{children}</main>
       <Footer />
     </div>
