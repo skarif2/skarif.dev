cbbcf2da44c99298a61faf97b3105e90da85c1aa
Sk Arif <skarif2@gmail.com>
2019-08-28 23:41:09 +0600

    working on search and categories

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { graphql } from "gatsby"
@@ -52,17 +52,79 @@ export const pageQuery = graphql`
 
 const BlogPage = props => {
   const { data } = props
-  const categories = data.categories.group
-  const posts = data.posts.edges
+  const [searchKey, setSearchKey] = useState("")
+  const [categories, setCategories] = useState([])
+  const [posts, setPosts] = useState(data.posts.edges)
+
+  useEffect(() => {
+    let filteredPosts = data.posts.edges.filter(post => {
+      return post.node.frontmatter.title
+        .toLowerCase()
+        .includes(searchKey.toLowerCase())
+    })
+    if (categories.length > 0) {
+      filteredPosts = filteredPosts.filter(
+        post =>
+          post.node.frontmatter.categories &&
+          categories.every(cat =>
+            post.node.frontmatter.categories.includes(cat)
+          )
+      )
+    }
+    setPosts(filteredPosts)
+  }, [searchKey, categories])
+
+  const handleChange = async event => {
+    const { value } = event.target
+    await setSearchKey(value)
+  }
+
+  const updateCategories = category => {
+    if (!categories.includes(category)) {
+      setCategories([...categories, category])
+    } else {
+      setCategories(categories.filter(cat => category !== cat))
+    }
+  }
+
+  const allCategories = data.categories.group
+  const totalCount = data.posts.edges.length
+  const filterCount = posts.length
+
   return (
     <Layout>
       <Helmet title={`Blogs | ${config.siteTitle}`} />
       <div className="container">
         <h1>Blogs</h1>
-        <div>
-          {categories.map(category => {
-            return <div key={category.fieldValue}>{category.fieldValue}</div>
+        <div className="category-container">
+          {allCategories.map(category => {
+            const active = categories.includes(category.fieldValue)
+
+            return (
+              <button
+                className={`category-filter ${active ? "active" : ""}`}
+                key={category.fieldValue}
+                type="button"
+                onClick={async () => {
+                  updateCategories(category.fieldValue)
+                }}
+              >
+                {category.fieldValue}
+              </button>
+            )
           })}
+        </div>
+        <div className="search-container">
+          <input
+            className="search"
+            type="text"
+            value={searchKey}
+            placeholder="Type here to filter posts..."
+            onChange={handleChange}
+          />
+          <div className="filter-count">
+            {filterCount} of {totalCount}
+          </div>
         </div>
         <PostList postEdges={posts} />
       </div>
